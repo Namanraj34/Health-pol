e5d4a0554d951af2eb54c49dafc48df9b59299c6
Namanraj34 <80189451+Namanraj34@users.noreply.github.com>
2022-04-04 16:09:23 +0530

    Add files via upload

---
+++ button-fun.js
@@ -0,0 +1,95 @@
+
+
+function myFunction() {
+    // for(i = 1;i<=10;i++){
+    //     document.write(`2 x ${i} = ${2*i}`)
+    //     document.write("<br>")
+    // }
+    var health = document.getElementById('health').value;
+    var age = document.getElementById('age').value;
+    var Location = document.getElementById('city').value;
+    var gender = document.getElementById('gender').value;
+
+    // let health = prompt("Enter Your Health 'Good or Poor'", "guest")
+    // let age = prompt("Enter Your Age", 20)
+    // let Location = prompt("Enter Your Location 'City or Village'", "City")
+    // let gender = prompt("Enter Your Gender", "Male or Female")
+
+    if (health == "Good" && age > 18 && Location == "City" && gender == "Male") {
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>"  Gender: ${gender}`)
+        document.write("<br>", "Your Health Policy is 35 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+    }
+
+    else if (health == "Poor" && age > 18 && Location == "City" && gender == "Male") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 25 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+
+    }
+
+    else if (health == "Good" && age > 18 && Location == "Village" && gender == "Male") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 31 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+
+    }
+    else if (health == "Poor" && age > 18 && Location == "Village" && gender == "Male") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 28 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+
+    }
+    else if (health == "Good" && age > 18 && Location == "City" && gender == "Female") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 33 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+
+    }
+    else if (health == "Poor" && age > 18 && Location == "City" && gender == "Female") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 28 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+
+    }
+
+    else if (health == "Good" && age > 18 && Location == "Village" && gender == "Female") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 30 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+
+    }
+    else if (health == "Poor" && age > 18 && Location == "Village" && gender == "Female") {
+
+        document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>" Gender: ${gender}`)
+
+        document.write("<br>","Your Health Policy is 28 Lakh Rupees '<br>'")
+        document.write("To Apply This Policy Please Contact Us =>  7485962547")
+    }
+
+    else {
+        document.write("You not field correctly the form '<br>'")
+
+  
+    }
+}
+
+window.localStorage
+// let sum = 0;
+
+        // for(i=1;i<=10;i++){
+        //     document.write(`${num} x ${i} = ${num*i}`)
+        //     document.write("<br>")
+        // }
