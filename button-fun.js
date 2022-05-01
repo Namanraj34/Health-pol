c25e3fc60b4516572428f06854131725d74a5f20
Namanraj34 <80189451+Namanraj34@users.noreply.github.com>
2022-05-02 00:06:01 +0530

    Update button-fun.js

--- button-fun.js
+++ button-fun.js
@@ -1,21 +1,11 @@
 
 
 function myFunction() {
-    // for(i = 1;i<=10;i++){
-    //     document.write(`2 x ${i} = ${2*i}`)
-    //     document.write("<br>")
-    // }
     var health = document.getElementById('health').value;
     var age = document.getElementById('age').value;
     var Location = document.getElementById('city').value;
     var Village = document.getElementById('village').value;
     var gender = document.getElementById('gender').value;
-
-    // let health = prompt("Enter Your Health 'Good or Poor'", "guest")
-    // let age = prompt("Enter Your Age", 20)
-    // let Location = prompt("Enter Your Location 'City or Village'", "City")
-    // let gender = prompt("Enter Your Gender", "Male or Female")
-
 
     if (health == "Good" && age > 18 && Location == "City" && gender == "Male") {
         document.write(`Your health is ${health} "<br>" Your Age is ${age} "<br>" Live in ${Location} "<br>"  Gender: ${gender}`)
